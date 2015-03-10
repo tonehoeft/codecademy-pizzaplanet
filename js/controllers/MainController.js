123a41ae85f570e756d545ddff998b57a6b78b43
Tone Hoeft <tone@tonehoeft.com>
2015-03-10 05:11:21 +0000

    Inital commit

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,6 +1,6 @@
 app.controller('MainController', ['$scope', function($scope) {
   $scope.today = new Date();
-
+  
   $scope.appetizers = [
     {
       name: 'Caprese',
@@ -11,6 +11,49 @@ app.controller('MainController', ['$scope', function($scope) {
       name: 'Mozzarella Sticks',
       description: 'Served with marinara sauce.',
       price: 3.95
+    },
+    {
+      name: 'Bruschetta',
+      description: 'Grilled bread garlic, tomatoes, olive oil.',
+      price: 4.95
+    }
+  ];
+  
+  $scope.mains = [
+    {
+      name: 'Margherita',
+      description: 'Marinara sauce, mozzarella, fresh basil.',
+      price: 6.95
+    },
+    {
+      name: 'Pepperoni',
+      description: 'Pepperoni, marinara sauce, mozzarella, fresh oregano.',
+      price: 7.95
+    },
+    {
+      name: 'Rustica',
+      description: 'Marinara sauce, kalmata olives, roasted garlic, fresh oregano.',
+      price: 8.95
+    }
+  ];
+  
+  $scope.name = "Tone";
+  
+  $scope.extras = [
+    {
+      name: 'Breadsticks',
+      description: 'Served with marinara sauce.',
+      price: 4.95
+    },
+    {
+      name: 'Soup of the day',
+      description: 'Take a chance.',
+      price: 4.95
+    },
+    {
+      name: 'Buffalo wings',
+      description: 'Feel the power of wings.',
+      price: 6.95
     }
   ];
 
